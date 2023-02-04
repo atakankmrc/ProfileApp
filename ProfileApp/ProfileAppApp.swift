6fb935496a6867c3e16193543ea6f5bd77b4f093
Atakan KÖMÜRCÜ <komurcu508@gmail.com>
2023-02-04 20:12:27 +0300

    Initial Commit

---
+++ ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by Atakan Kömürcü on 4.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
